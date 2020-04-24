3d65d0c352790656fead5caebd64e7f2ca3b8068
Bruno Konrad <bruno.skon@gmail.com>
2020-04-24 15:36:02 +0200

    A better jumping

--- Actors/Player/Jump.gd
+++ Actors/Player/Jump.gd
@@ -1,5 +1,18 @@
 extends "res://Actors/Player/KingState.gd"
 
+var current_time = 0
+var released = false
+
+func state_logic(delta):
+	current_time += delta
+
+	if _should_increase_jump():
+		host().velocity.y -= 85
+
+	if Input.is_action_just_released("jump"):
+		print("Release ", current_time)
+		released = true
+	
 func get_transition(delta):
 	if host().is_falling():
 		return "fall"
@@ -7,6 +20,21 @@ func get_transition(delta):
 		return "idle"
 
 func enter_state(previous_state):
-	var host = host()
+	_reset()
 	
-	host.velocity.y = -host.jump_strength
+	host().velocity.y = -350
+	
+func exit_state(x):
+	print("Exit ", current_time)
+	
+func _reset():
+	current_time = 0
+	released = false
+
+func _should_increase_jump():
+	return (
+		Input.is_action_pressed("jump") and
+		current_time >= 0.05 and
+		current_time <= 0.125 and
+		not released
+	)
